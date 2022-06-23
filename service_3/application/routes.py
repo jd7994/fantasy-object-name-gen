eb94d735b3eedd3b09050c32524a974d6f8d462f
Jon <jderbyshire67@gmail.com>
2022-06-23 15:42:13 +0000

    framework of app in place

---
+++ service_3/application/routes.py
@@ -0,0 +1,6 @@
+from application import app
+from random import randint
+
+@app.route('INSERT ROUTE HERE', methods=['GET'])
+#PUT IT HERE
+#essentially we just return a value and then it'll be captured in routes of service 1
