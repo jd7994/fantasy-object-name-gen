90002b43533370fd0cc0a0dd46c2f0b1d024de61
Jon <jderbyshire67@gmail.com>
2022-06-27 15:32:44 +0000

    Displaying error to web page

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -10,7 +10,7 @@ def index():
     source = requests.get('http://service_3:5000/rand_2').text
     name = requests.post('http://service_4:5000/prize', json=dict(obj=obj, source=source))
 
-    return render_template('home.html', name = name.text)
+    return render_template('home.html', name = name.text, obj = obj, source = source)
     #rand_1 = requests.get('http://service_2:5000/rand_1')
     #final = requests.post('http://service_4:5000/final)
     #because it's a post request it also accepts info back 
