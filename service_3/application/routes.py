30a866f26b05e8d7cb0cef96ae5136863463cb7c
Jon <jderbyshire67@gmail.com>
2022-07-08 10:08:07 +0000

    Second iteration/webhook test

--- service_3/application/routes.py
+++ service_3/application/routes.py
@@ -2,7 +2,7 @@ from application import app
 from random import choice
 
 @app.route('/rand_2', methods=['GET'])
-def rand_2(): #changethis
+def rand_2(): 
     sources = ["flame", "earth", "water", "air", 'the celestial', "hellfire", "the occult", "the infernal", "the void", "fey", "shadow", "enchantment", "illusion", "abberation"]
     selection = choice(sources)
     return selection
